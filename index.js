d6edfc229b152eb6b1f38a6ef2a4026d9cbc3413
HHieu2003 <hieutran170626@gmail.com>
2025-03-29 02:00:46 +0700

    alst

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const forumRoutes = require('./routes/forum');
 const profileRoutes = require('./routes/profile');
+require('dotenv').config(); // Tải biến môi trường từ .env
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-// Kết nối MongoDB
-mongoose.connect('mongodb://localhost:27017/studentmanagement', {
+
+// Kết nối MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('Kết nối MongoDB thành công'))
-  .catch(err => console.log('Lỗi kết nối MongoDB:', err));
+  .then(() => console.log('Kết nối MongoDB Atlas thành công'))
+  .catch(err => console.log('Lỗi kết nối MongoDB Atlas:', err));
+
 
 // Middleware
 app.use(express.json());
