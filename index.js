6d63e5a264c13bfdf29c9793f388b2637135462a
HHieu2003 <hieutran170626@gmail.com>
2025-03-30 15:50:23 +0700

    Cập nhật file index.js - sửa lỗi dotenv

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const userRoutes = require('./routes/users');
 const forumRoutes = require('./routes/forum');
 const profileRoutes = require('./routes/profile');
 
-const app = express();
-const port = 3000;
 
-// Kết nối MongoDB
-mongoose.connect('mongodb://localhost:27017/studentmanagement', {
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Kết nối MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('Kết nối MongoDB thành công'))
-  .catch(err => console.log('Lỗi kết nối MongoDB:', err));
+  .then(() => console.log('Kết nối MongoDB Atlas thành công'))
+  .catch(err => console.log('Lỗi kết nối MongoDB Atlas:', err));
 
 // Middleware
 app.use(express.json());
